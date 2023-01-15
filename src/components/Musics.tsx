c3f3a5f7cc6746c77784607dc4dcd60068c62d22
Adimar Sarkis <sarkis170@gmail.com>
2023-01-15 17:55:51 -0300

    Algumas atualizações

--- src/components/Musics.tsx
+++ src/components/Musics.tsx
@@ -1,42 +1,35 @@
-import {CheckCircle, Lock} from 'phosphor-react'
+import {Headphones} from 'phosphor-react'
 import { Link } from 'react-router-dom';
+
+
 
 
 interface MusicProps {
   title: string;
   slug: string;
   avatarUrl:string;
-  name: string;
+  name: string; 
 }
 
 
-
 export function Musics(props: MusicProps){
-const isLessonAvailable = true;
+
   return(
     <Link to={`/music/${props.slug}`} className='group'>
-      <span className="text-gray-300">
-        {props.name}
-      </span>
+      
 
       <div className="rounded border group-hover:border-green-500 border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          {isLessonAvailable ? (
+          
             <span className="text-sm text-blue-500 font-medium
               flex gap-2 items-center">
-              <CheckCircle size={20}/>
-              Conteudo liberado
+              <Headphones size={20} />
             </span>
-          ): (
-            <span className="text-sm text-orange-500 font-medium
-              flex gap-2 items-center">
-            <Lock size={20}/>
-            Em breve
-          </span>
-          )}
+          
+           
           <span className="text-xs rounded px-2 py-[0.125rem]
           font-bold border border-green-300 text-white">
-            Ao vivo
+            Geek
           </span>
         </header>
 
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,5 @@
 import { Player, Youtube, DefaultUi } from "@vime/react";
-import {DiscordLogo, Lightning,CaretRight,FileArrowDown} from "phosphor-react";
+import {GithubLogo, LinkedinLogo,CaretRight,SpotifyLogo,YoutubeLogo} from "phosphor-react";
 import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css';
@@ -15,6 +15,8 @@ const GET_MUSIC_BY_SLUG_QUERY = gql`
         bio
         name
         avatarUrl
+        youtubeUrl
+        spotifyUrl
       }
     }
   }
@@ -29,6 +31,8 @@ interface GetMusicBySlugResponse{
       bio: string;
       name: string;
       avatarUrl: string;
+      youtubeUrl: string;
+      spotifyUrl: string;
     }
   }
 }
@@ -71,10 +75,6 @@ export function Video(props: VideoProps) {
             <h1 className="text-2xl font-bold">
               {data.music.title}
             </h1>
-            <p className="text-gray-200 mt-4 leading-relaxed">
-              {data.music.description}
-            </p>
-
             <div className="flex items-center gap-4 mt-6">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
@@ -90,36 +90,43 @@ export function Video(props: VideoProps) {
                 </span>
               </div>
             </div>
+            <p className="text-gray-200 mt-4 leading-relaxed">
+              {data.music.description}
+            </p>
+
           </div>
           <div className="flex flex-col gap-4">
-            <a href="" className="p-4 text-small bg-green-500
+            <a href={data.music.artista.spotifyUrl} target="_blank"
+            className="p-4 text-small bg-green-500
               flex items-center rounded font-bold uppercase gap-2
               justify-center hover:bg-green-700 transition-colors">
-                <DiscordLogo size={24}/>
-                Comunidade do discord
+                <SpotifyLogo size={24} />
+                Perfil do Spotify
             </a>
-            <a href="" className="p-4 text-small border border-blue-500
+            <a href={data.music.artista.youtubeUrl} target="_blank"
+            className="p-4 text-small border border-blue-500
               flex items-center rounded font-bold uppercase gap-2
               justify-center text-blue-500 hover:bg-blue-500 hover:text-gray-900 transition-colors">
-                <Lightning size={24}/>
-                Acesse o desafio
+                <YoutubeLogo size={24}/>
+                Acesse o canal do artista
             </a>
           </div>
         </div>
 
 
         <div className="gap-8 mt-20 grid grid-cols-2">
-          <a href="" className="bg-gray-700 overflow-hidden rounded
+          <a href="https://github.com/AdimarSarkis" target="_blank"
+           className="bg-gray-700 overflow-hidden rounded
             flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
               <div className="bg-green-700 h-full p-6 flex items-center">
-                <FileArrowDown size={40}/>
+                <GithubLogo size={40} />
               </div>
               <div className="py-6 leading-relaxed">
                 <strong className="text-2xl">
-                  Material complentar
+                  Github
                 </strong>
                 <p className="text-sm text-gray-200 mt-2">
-                  Acesse o material complentar para acelerar o seu desenvolvimento
+                  Acesse o meu perfil do github para visualizar outros projetos
                 </p>
               </div>
               <div className="h-full p-6 flex items-center">
@@ -127,17 +134,18 @@ export function Video(props: VideoProps) {
               </div>
           </a>
 
-          <a href="" className="bg-gray-700 overflow-hidden rounded
+          <a href="linkedin.com/in/adimar-sarkis-21b3a6239/" target="_blank"
+          className="bg-gray-700 overflow-hidden rounded
             flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
               <div className="bg-green-700 h-full p-6 flex items-center">
-                <FileArrowDown size={40}/>
+              <LinkedinLogo size={40}/>
               </div>
               <div className="py-6 leading-relaxed">
                 <strong className="text-2xl">
-                  Wallpapers
+                  Linkedin
                 </strong>
                 <p className="text-sm text-gray-200 mt-2">
-                  Baixe wallpapers exclusivos do Iginite Lab
+                  Entre no meu linkedin e conecte-se comigo
                 </p>
               </div>
               <div className="h-full p-6 flex items-center">
