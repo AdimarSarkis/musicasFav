706d4393cf1b0982a75159f3e19a22878a2be02a
Adimar Sarkis <sarkis170@gmail.com>
2023-01-15 18:02:54 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+/* import ReactDOM from 'react-dom/client'*/
 import App from './App'
 
-
+import { render } from "react-dom";
 import './styles/global.css';
-
+/*
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       <App />
   </React.StrictMode>
-)
+)*/
+const root = document.getElementById("root");
+render(<App />, root);
